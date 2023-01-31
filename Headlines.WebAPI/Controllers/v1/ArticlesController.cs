ded9ea8c7502f5abb265b54ef80ddbbdd585b576
PetrBilek1 <80153579+PetrBilek1@users.noreply.github.com>
2023-01-31 14:34:56 +0100

    added request checks to controller

--- Headlines.WebAPI/Controllers/v1/ArticlesController.cs
+++ Headlines.WebAPI/Controllers/v1/ArticlesController.cs
@@ -1,4 +1,7 @@
-﻿using Headlines.WebAPI.Contracts.V1.Requests.Articles;
+﻿using Headlines.BL.Facades;
+using Headlines.DTO.Entities;
+using Headlines.WebAPI.Contracts.V1.Requests.Articles;
+using Headlines.WebAPI.Contracts.V1.Responses.Articles;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Headlines.WebAPI.Controllers.v1
@@ -8,13 +11,44 @@ namespace Headlines.WebAPI.Controllers.v1
     [ApiVersion("1.0")]
     public sealed class ArticlesController : ControllerBase
     {
+        private readonly IArticleSourceFacade _articleSourceFacade;
+
         public const int MaxTake = 50;
         public const int DefaultTake = 10;
+
+        public ArticlesController(IArticleSourceFacade articleSourceFacade)
+        {
+            _articleSourceFacade = articleSourceFacade;
+        }
 
         [HttpPost("GetSkipTake")]
         public async Task<IActionResult> GetSkipTake([FromBody] GetSkipTakeRequest request, CancellationToken cancellationToken)
         {
-            return Ok();
+            request.Skip ??= 0;
+            request.Take ??= DefaultTake;
+            request.Take = Math.Min(request.Take.Value, MaxTake);
+
+            if (request.ArticleSources == null)
+            {
+                List<ArticleSourceDTO> sources = await _articleSourceFacade.GetAllArticleSourcesAsync(cancellationToken);
+                request.ArticleSources = sources.Select(x => x.Id).ToArray();
+            }
+            if (!request.ArticleSources.Any())
+            {
+                return Ok(new GetSkipTakeResponse
+                {
+                    Articles = new(),
+                    MatchesFiltersCount = 0
+                });
+            }               
+
+
+
+            return Ok(new GetSkipTakeResponse
+            {
+                Articles = new(),
+                MatchesFiltersCount = 0
+            });
         }
     }
 }
