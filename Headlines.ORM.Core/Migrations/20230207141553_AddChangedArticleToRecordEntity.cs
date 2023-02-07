e567a4ea7e657b3754d6374678de852b674420c3
PetrBilek1 <80153579+PetrBilek1@users.noreply.github.com>
2023-02-07 15:16:26 +0100

    added migration

---
+++ Headlines.ORM.Core/Migrations/20230207141553_AddChangedArticleToRecordEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Headlines.ORM.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChangedArticleToRecordEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UPDATED",
+                table: "ARTICLE",
+                newName: "CREATED");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CHANGED",
+                table: "ARTICLE",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CHANGED",
+                table: "ARTICLE");
+
+            migrationBuilder.RenameColumn(
+                name: "CREATED",
+                table: "ARTICLE",
+                newName: "UPDATED");
+        }
+    }
+}
