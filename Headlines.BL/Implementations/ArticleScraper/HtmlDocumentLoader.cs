274ffd62cf9e3cce24c60f293d92be3cded72a8b
PetrBilek1 <80153579+PetrBilek1@users.noreply.github.com>
2023-03-04 17:50:59 +0100

    added timeout to article scraper

--- Headlines.BL/Implementations/ArticleScraper/HtmlDocumentLoader.cs
+++ Headlines.BL/Implementations/ArticleScraper/HtmlDocumentLoader.cs
@@ -18,6 +18,8 @@ namespace Headlines.BL.Implementations.ArticleScraper
         {
             var client = _httpClientFactory.CreateClient();
 
+            client.Timeout = TimeSpan.FromSeconds(10);
+
             using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
 
             requestMessage.Headers.TryAddWithoutValidation("ACCEPT", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
