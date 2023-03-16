c17eda29b8072f8b61057ad991202913a6c683b4
PetrBilek1 <80153579+PetrBilek1@users.noreply.github.com>
2023-03-16 13:12:39 +0100

    added defaultarticlescraper unit test

---
+++ Headlines.BL.Tests/Implementations/ArticleScraper/DefaultArticleScraperTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Headlines.BL.Exceptions;
+using Headlines.BL.Implementations.ArticleScraper;
+using Xunit;
+
+namespace Headlines.BL.Tests.Implementations.ArticleScraper
+{
+    public sealed class DefaultArticleScraperTests
+    {
+        private readonly DefaultArticleScraper _sut = new();
+
+        [Fact]
+        public async Task ScrapeArticleAsync()
+        {
+            //Act
+            Func<Task> act = async () => await _sut.ScrapeArticleAsync("url");
+
+            //Assert
+            await act.Should().ThrowAsync<NotImplementedException>();
+        }
+    }
+}
